deb2fade0a75cc9cd5d57fc498da4dc69912378a
Noelson Rosa <41403798+Noelson-Rosa@users.noreply.github.com>
2024-01-26 19:02:57 -0300

    metodo

---
+++ sistema-smart-tv/src/SmartTv.java
@@ -0,0 +1,32 @@
+public class SmartTv{
+
+boolean ligada = false;
+int canal = 1;
+int volume = 25;
+
+public void mudarCanal(int novoCanal){
+    canal = novoCanal;
+}
+
+
+
+public void aumentarVolume(){
+    volume++;
+    System.out.println("aumentando o volume " + volume);
+}
+
+public void diminuirVolume(){
+    volume--;
+    System.out.println("diminuindo o volume " + volume);
+}
+
+
+public void ligar(){
+    ligada = true;
+}
+
+public void desligar(){
+    ligada = false;
+}
+
+}
